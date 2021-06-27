88c0fbc7a8674ff47ae571ba965cef685628b8b0
huan.huynh <huan.huynh@linecorp.com>
2021-06-27 17:16:00 +0700

    add handlebars

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const morgan = require('morgan')
+const handlebars = require('express-handlebars')
+const path = require('path')
+
+const app = express()
+const PORT= 3000
+
+
+// Http logger
+app.use(morgan('combined'))
+
+// Template engine
+app.engine('hbs', handlebars({
+    extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+})
