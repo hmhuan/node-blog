ad650d13dd1c7688cf9e067c5ccf1c6dc9cd62b5
Huan <minhhuanhuynh289@gmail.com>
2021-07-03 10:23:50 +0700

    check bad request

--- src/services/beverage.service.js
+++ src/services/beverage.service.js
@@ -17,14 +17,18 @@ class BeverageService {
     }
 
     findById(req, res, next) {
-        Beverage.findById(req.params.id)
-        .then(beverage => {
-            if (beverage) {
-                return res.json(beverage)
-            }
-            return res.status(404).json({message: "Not found"})
-        })
-        .catch(next)
+        
+        return Beverage.findById(req.params.id)
+            .then(beverage => {
+                if (beverage) {
+                    return res.json(beverage)
+                }
+                return res.status(404).json({message: "Not found"})
+            })
+            .catch(error => {
+                console.log(`error`)
+                return res.status(400).json({message: "Bad request"})
+            })
     }
 }
 
